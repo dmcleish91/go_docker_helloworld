c8fecf36ecbcf27249c8ca495b6f470ba497d6da
Dwight Mcleish Jr <dmcleishjr@gmail.com>
2024-09-17 04:21:59 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, Docker on Raspberry Pi!")
+}
+
+func main() {
+    http.HandleFunc("/", hello)
+    fmt.Println("Server starting on port 8080...")
+    http.ListenAndServe(":8080", nil)
+}
